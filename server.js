3a6dba6a5fb0eec31e7e69a8d8c7689e05c46ed7
Jay7735 <jaydeepdhillon@hotmail.co.uk>
2023-04-24 15:51:48 +0100

    working on q5

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const fs = require("fs/promises");
 
 const server = http.createServer((request, response) => {
- // console.log('Hello')
+  // console.log('Hello')
   const { method, url } = request;
   if (method === "GET" && url === "/api") {
     response.statusCode = 200;
@@ -15,47 +15,66 @@ const server = http.createServer((request, response) => {
       const books = JSON.parse(data);
       response.statusCode = 200;
       response.setHeader("Content-Type", "application/json");
-      response.write(JSON.stringify({books: books}));
+      response.write(JSON.stringify({ books: books }));
       response.end();
     });
-  } else if (method === "GET" && url === "/api/authors"){
+  } else if (method === "GET" && url === "/api/authors") {
     fs.readFile("./data/authors.json").then((data) => {
       const authors = JSON.parse(data);
       response.statusCode = 200;
       response.setHeader("Content-Type", "application/json");
-      response.write(JSON.stringify({authors: authors}));
+      response.write(JSON.stringify({ authors: authors }));
       response.end();
     });
-  } else if (method === "GET" &&  url.slice(0,11) === "/api/books/"){
-  
-  const bookIdNum =Number(url.slice(11,url.length));
+  } else if (method === "GET" && url.slice(0, 11) === "/api/books/") {
+    const bookIdNum = Number(url.slice(11, url.length));
 
     fs.readFile("./data/books.json").then((data) => {
       const books = JSON.parse(data);
-     // console.log(books)
+      // console.log(books)
       const book = books.filter((book) => {
         return book.bookId === bookIdNum;
-      })
-      let object={};
-      if(book.length<1) {object="Error 404 : Book not found"
-          }
-          else{
-            object.book=book
-          }
-      console.log(book)
+      });
+      let object = {};
+      if (book.length < 1) {
+        object = "Error 404 : Book not found";
+      } else {
+        object.book = book;
+      }
+      console.log(book);
       response.statusCode = 200;
       response.setHeader("Content-Type", "application/json");
       response.write(JSON.stringify(object));
       response.end();
-    })
-  
+    });
+  } else if (method === "POST" && url === "/api/books") {
+    let body = "";
+    request.on("data", (packet) => {
+      body += packet.toString();
+      request.on("end", () => {
+        const newBook = JSON.parse(body);
+        //console.log(newBook);
+        fs.readFile('./data/books.json')
+        .then((fileContents) => {
+            const booksArr = JSON.parse(fileContents)
+            console.log(booksArr)
+            const {...books, booksArr  } = books
+            return fs.writeFile('./data/books.json', JSON.stringify(books, null, 2))
+            .then(()=>{
+                response.setHeader('Content-Type', 'applications/json')
+                response.statusCode = 201
+                response.write(JSON.stringify(books, null, 2))
+                response.end()
+            })
+        })
+      });
+    });
   }
-  
-   // response.statusCode=404;
-   // response.setHeader("Content-Type", "application/json");
-    //  response.write(JSON.stringify({Error:"404 not found"}));
-    //  response.end();
-        
+
+  // response.statusCode=404;
+  // response.setHeader("Content-Type", "application/json");
+  //  response.write(JSON.stringify({Error:"404 not found"}));
+  //  response.end();
 });
 
 server.listen(9090, (error) => {
